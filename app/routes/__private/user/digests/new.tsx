3d94310348fe6deee8af35a301af234510ab1d69
David Vargas <dvargas92495@gmail.com>
2022-05-12 00:23:25 -0500

    table fix and textarea

--- app/routes/__private/user/digests/new.tsx
+++ app/routes/__private/user/digests/new.tsx
@@ -1,4 +1,5 @@
 import Button from "@dvargas92495/app/components/Button";
+import Textarea from "@dvargas92495/app/components/Textarea";
 import TextInput from "@dvargas92495/app/components/TextInput";
 import { Form } from "@remix-run/react";
 import { ActionFunction, redirect } from "@remix-run/node";
@@ -8,8 +9,12 @@ import createDigest from "../../../../data/createDigest.server";
 const NewDigestPage = () => {
   return (
     <Form method="post" className="max-w-xl">
-      <TextInput label={"Name"} name={"name"} />
-      <TextInput label={"Description"} name={"description"} />
+      <TextInput label={"Name"} name={"name"} placeholder={"Weekly Digest"} />
+      <Textarea
+        label={"Description"}
+        name={"description"}
+        placeholder={"Bringing users the latest updates"}
+      />
       <Button>Submit</Button>
     </Form>
   );
