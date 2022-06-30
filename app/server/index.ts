004b32380de48d261e2d8537a7515729b8db041d
David Vargas <dvargas92495@gmail.com>
2022-06-30 01:24:21 -0400

    fix remix handler

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,2 +1,2 @@
-import getRemixHandler from "@dvargas92495/api/getRemixHandler";
+import getRemixHandler from "@dvargas92495/app/backend/getRemixHandler.server";
 export const handler = getRemixHandler();
